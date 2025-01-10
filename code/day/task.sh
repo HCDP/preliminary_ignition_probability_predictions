cb542da27732b0120525a2675b74da881cfc6d1f
kodamak-hi <kodamak8@hawaii.edu>
2025-01-10 11:49:24 -1000

    KMK: added main Dockerfile, edit config dirs

--- code/day/task.sh
+++ code/day/task.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-echo "Fetch dependencies, all counties"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/wget_dependencies.py 2024-12-31
+echo "[task.sh] [1/5] Starting Execution."
+export TZ="HST"
+echo "It is currently $(date)."
+if [ $CUSTOM_DATE ]; then
+    echo "An aggregation date was provided by the environment."
+else
+    export CUSTOM_DATE=$(date -d "1 day ago" --iso-8601)
+    echo "No aggregation date was provided by the environment. Defaulting to yesterday."
+fi
+echo "Aggregation date is: " $CUSTOM_DATE
 
-echo "Generate API, all counties"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py bi 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py ka 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py mn 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py oa 2024-12-31
+echo "[task.sh] [2/5] Fetching dependencies, all counties."
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/wget_dependencies.py $CUSTOM_DATE
 
-echo "Run map script for all counties"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py bi 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py ka 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py mn 2024-12-31
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py oa 2024-12-31
+echo "[task.sh] [3/5] Generating API, all counties."
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py bi $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py ka $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py mn $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_api.py oa $CUSTOM_DATE
 
-echo "Create statewide mosaic"
+echo "[task.sh] [4/5] Running map workflow, all counties"
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py bi $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py ka $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py mn $CUSTOM_DATE
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/generate_ignition_map.py oa $CUSTOM_DATE
+
+echo "[task.sh] [5/5] Creating statewide mosaic."
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/ignition_prob/code/day/statewide_mosaic.py
