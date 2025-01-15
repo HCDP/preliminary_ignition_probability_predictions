8057aaaa76dd908fe5f1a92192fc3220121007ee
jardmclan <mcleanj@hawaii.edu>
2025-01-14 14:12:29 -1000

    fix paths

--- containers/day/task.sh
+++ containers/day/task.sh
@@ -10,25 +10,25 @@ else
     echo "No aggregation date was provided by the environment. Defaulting to yesterday."
 fi
 echo "Aggregation date is: " $CUSTOM_DATE
-source envs/prod.env
+source /workspace/envs/prod.env
 
 echo "[task.sh] [2/7] Fetching dependencies, all counties."
-python3 -W ignore /code/wget_dependencies.py $CUSTOM_DATE
+python3 -W ignore /workspace/code/wget_dependencies.py $CUSTOM_DATE
 
 echo "[task.sh] [3/7] Generating API, all counties."
-python3 -W ignore /code/generate_api.py bi $CUSTOM_DATE
-python3 -W ignore /code/generate_api.py ka $CUSTOM_DATE
-python3 -W ignore /code/generate_api.py mn $CUSTOM_DATE
-python3 -W ignore /code/generate_api.py oa $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_api.py bi $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_api.py ka $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_api.py mn $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_api.py oa $CUSTOM_DATE
 
 echo "[task.sh] [4/7] Running map workflow, all counties"
-python3 -W ignore /code/generate_ignition_map.py bi $CUSTOM_DATE
-python3 -W ignore /code/generate_ignition_map.py ka $CUSTOM_DATE
-python3 -W ignore /code/generate_ignition_map.py mn $CUSTOM_DATE
-python3 -W ignore /code/generate_ignition_map.py oa $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_ignition_map.py bi $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_ignition_map.py ka $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_ignition_map.py mn $CUSTOM_DATE
+python3 -W ignore /workspace/code/generate_ignition_map.py oa $CUSTOM_DATE
 
 echo "[task.sh] [5/7] Creating statewide mosaic."
-python3 -W ignore /code/statewide_mosaic.py
+python3 -W ignore /workspace/code/statewide_mosaic.py
 
 echo "[task.sh] [6/7] Preparing upload config."
 cd /sync
