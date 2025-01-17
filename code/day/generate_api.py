f9985914be1ecff27d4355b515fe792fa5eb33c2
jardmclan <mcleanj@hawaii.edu>
2025-01-16 17:17:34 -1000

    add retries for raster retreival functions, move redundant code to functions, move static dependency files to container tar

--- code/day/generate_api.py
+++ code/day/generate_api.py
@@ -7,55 +7,62 @@ from dateutil import parser
 import pytz
 import sys
 import os
+from util import handle_retry
 
 hcdp_api_token = os.environ.get('HCDP_API_TOKEN')
 num_agg_maps = 180
 
-def generate_api_k1(agg_date,extent,n_agg=180):
+def aggregate_map(date, extent, repeat_times, agg_map, n_agg):
+    found = False
+    date_s = date.strftime("%Y-%m-%d")
+    #URL for API raster request
+    raster_url = f"https://api.hcdp.ikewai.org/raster?date={date_s}&extent={extent}&datatype=rainfall&production=new&period=day"
+    #prepare request
+    req = urllib.request.Request(raster_url)
+    #add auth header to request
+    req.add_header("Authorization", f"Bearer {hcdp_api_token}")
+    try:
+        #open remote file
+        with urllib.request.urlopen(req, timeout = 5) as f:
+            #wrap file handle in rasterio memory file
+            with MemoryFile(f) as mem_f:
+                #open the memory file as a rasterio object
+                with mem_f.open() as data:
+                    #initialize agg map if first one
+                    if agg_map is None:
+                        #set agg map to current map's first band (mask nodata)
+                        agg_map = data.read(1, masked = True)
+                        #less one map and repeat
+                        n_agg -= 1
+                        repeat_times -= 1
+                    #repeat the file until max reached or repeats are exhausted
+                    while repeat_times > 0 and n_agg > 0:
+                        #add current map to aggregation map
+                        agg_map += data.read(1, masked = True)
+                        #less one map and repeat
+                        n_agg -= 1
+                        repeat_times -= 1
+    except urllib.error.HTTPError as e:
+        #file was not found, repeat the next file an additional time
+        if e.code != 404:
+            raise e
+    return (found, n_agg, agg_map)
+
+#reset repeat to 1
+# repeat_times = 1
+
+def generate_api_k1(agg_date, extent, n_agg = 180):
     #Generates the [n_agg] sum of rainfall (default 180 days)
     repeat_times = 1
     agg_map = None
     #clone agg date as starting date
     date = agg_date.replace()
     while n_agg > 0:
-        #date = date - timedelta(days = 1) #commenting out position so that loop starts on agg_date
-        date_s = date.strftime("%Y-%m-%d")
-        print(f"Adding {date_s} to API")
-        #URL for API raster request
-        raster_url = f"https://api.hcdp.ikewai.org/raster?date={date_s}&extent={extent}&datatype=rainfall&production=new&period=day"
-        #prepare request
-        req = urllib.request.Request(raster_url)
-        #add auth header to request
-        req.add_header("Authorization", f"Bearer {hcdp_api_token}")
-        try:
-            #open remote file
-            with urllib.request.urlopen(req) as f:
-                #wrap file handle in rasterio memory file
-                with MemoryFile(f) as mem_f:
-                    #open the memory file as a rasterio object
-                    with mem_f.open() as data:
-                        #initialize agg map if first one
-                        if agg_map is None:
-                            #set agg map to current map's first band (mask nodata)
-                            agg_map = data.read(1, masked = True)
-                            #less one map and repeat
-                            n_agg -= 1
-                            repeat_times -= 1
-                        #repeat the file until max reached or repeats are exhausted
-                        while repeat_times > 0 and n_agg > 0:
-                            #add current map to aggregation map
-                            agg_map += data.read(1, masked = True)
-                            #less one map and repeat
-                            n_agg -= 1
-                            repeat_times -= 1
-                        #reset repeat to 1
-                        repeat_times = 1
-        except urllib.error.HTTPError as e:
-            #file was not found, repeat the next file an additional time
-            if e.code == 404:
-                repeat_times += 1
-            else:
-                raise e
+        found, n_agg, agg_map = handle_retry(aggregate_map, (date, extent, repeat_times, agg_map, n_agg))
+        if(found):
+            repeat_times = 1
+        else:
+            repeat_times += 1
         date = date - timedelta(days = 1)
     return agg_map
     
@@ -74,7 +81,7 @@ if __name__ == "__main__":
         today = datetime.today().astimezone(hst)
         agg_date = today - timedelta(days = 1)
 
-    api = generate_api_k1(agg_date,extent)
+    api = generate_api_k1(agg_date, extent, num_agg_maps)
     local_dep_dir = os.environ.get('DEPENDENCY_DIR')
     with rasterio.open(local_dep_dir + 'ref_'+extent+'.tif','r') as raster:
         profile = raster.profile
--- code/day/wget_dependencies.py
+++ code/day/wget_dependencies.py
@@ -1,17 +1,18 @@
 import os
 import sys
-import subprocess
 import pytz
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
+from util import handle_retry
+from os.path import join
 
-#paste into env 'https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/'
-public_url = os.environ.get('IKEWAI_BASE')
 local_dep_dir = os.environ.get('DEPENDENCY_DIR')
 hcdp_api_token = os.environ.get('HCDP_API_TOKEN')
 county_list = ['bi','ka','mn','oa']
+datasets = [({"datatype": "ndvi_modis"}, "NDVI"), ({"datatype": "rainfall", "production": "new"}, "Preciptation"), ({"datatype": "temperature", "aggregation": "max"}, "Tmax"), ({"datatype": "relative_humidity"}, "RH")]
 
+targ_dt = None
 if len(sys.argv) > 1:
     #Set input to specific date
     target_date = sys.argv[1]
@@ -22,105 +23,41 @@ else:
     today = datetime.today().astimezone(hst)
     targ_dt = today - timedelta(days = 1)
 
+def dataset2params(dataset):
+    "&".join("=".join(item) for item in dataset.items())
+
+def get_raster(date, county, dataset, outf):
+    date_s = date.strftime('%Y-%m-%d')
+    ndvi_url = f"https://api.hcdp.ikewai.org/raster?date={date_s}&extent={county}&{dataset2params(dataset)}&returnEmptyNotFound=False"
+    found = False
+    try:
+        req = requests.get(ndvi_url,headers={'Authorization':f'Bearer {hcdp_api_token}'}, timeout = 5)
+        req.raise_for_status()
+        with open(outf, 'wb') as f:
+            f.write(req.content)
+        found = True
+    except requests.exceptions.HTTPError as e:
+        if e.code != 404:
+            raise e
+    return found
+
+#do any of the other processes need to know what the dates of these files are??
+def get_last_raster(date, county, dataset, outf):
+    #should this only go back 10 days max? Is it possible for NDVI to be more out of date?
+    init_date = date.strftime('%Y-%m-%d')
+    MAX_DAYS = 10
+    found = False
+    for i in range(0, MAX_DAYS):
+        found = handle_retry(get_raster, (date, county, dataset, outf))
+        if(found):
+            break
+        date -= timedelta(days = 1)
+    if(not found):
+        raise Exception(f"No data for {dataset} found within {MAX_DAYS} of {init_date}")
+    return date
+    
 
 for county in county_list:
-    #import statics (model training)
-    #-- land cover
-    lc_url = public_url + 'workflow_data/preliminary/ignition_prob/dependencies/' + 'PerCov2016model_'+county+'.tif'
-    local_name = local_dep_dir + 'land_cover_'+county+'.tif'
-    cmd = ["wget",lc_url,"-O",local_name]
-    lc_rtn_code = subprocess.run(cmd).returncode
-    #-- training dataset
-    tr_url = public_url + 'workflow_data/preliminary/ignition_prob/dependencies/' + 'FireData-2002-2019_'+county+'.csv'
-    local_name = local_dep_dir + 'firedata_'+county+'.csv'
-    cmd = ["wget",tr_url,"-O",local_name]
-    tr_rtn_code = subprocess.run(cmd).returncode
-    #-- reference geotiff
-    ref_url = public_url + 'workflow_data/preliminary/ignition_prob/dependencies/' + 'ref_'+county+'.tif'
-    local_name = local_dep_dir + 'ref_'+county+'.tif'
-    cmd = ["wget",ref_url,"-O",local_name]
-    ref_rtn_code = subprocess.run(cmd).returncode #check that this executes cmd correctly
-
-    #import daily updated data
-    #--ndvi
-    date_fmt = targ_dt.strftime('%Y-%m-%d')
-    finish_flag = 0
-    attempts = 0
-    print(f'Getting NDVI for {date_fmt}')
-    while (finish_flag < 1)&(attempts<10):
-        ndvi_url = f"https://api.hcdp.ikewai.org/raster?date={date_fmt}&extent={county}&datatype=ndvi_modis&period=day&returnEmptyNotFound=False"
-        ndvi_file = local_dep_dir + 'NDVI_'+county+'.tif'
-        try:
-            req = requests.get(ndvi_url,headers={'Authorization':f'Bearer {hcdp_api_token}'})
-            req.raise_for_status()
-            with open(ndvi_file,'wb') as f:
-                f.write(req.content)
-            finish_flag = 1
-            attempts += 1
-        except requests.exceptions.HTTPError as err:
-            print(f'NDVI for {date_fmt} not found. Fetching previous day.')
-            dt = pd.to_datetime(date_fmt) - pd.Timedelta(days=1)
-            date_fmt = dt.strftime('%Y-%m-%d')
-
-    #--precip (Preciptation.tif)
-    #Reset loop flag and date
-    date_fmt = targ_dt.strftime('%Y-%m-%d')
-    finish_flag = 0
-    attempts = 0
-    print(f'Getting precip for {date_fmt}')
-    while (finish_flag < 1)&(attempts<10):
-        prec_url = f"https://api.hcdp.ikewai.org/raster?date={date_fmt}&extent={county}&datatype=rainfall&production=new&period=day&returnEmptyNotFound=False"
-        prec_file = local_dep_dir + 'Preciptation_'+county+'.tif'
-        try:
-            req = requests.get(prec_url,headers={'Authorization':f'Bearer {hcdp_api_token}'})
-            req.raise_for_status()
-            with open(prec_file,'wb') as f:
-                f.write(req.content)
-            finish_flag = 1
-            attempts += 1
-        except requests.exceptions.HTTPError as err:
-            print(f'Precipitation for {date_fmt} not found. Fetching previous day.')
-            dt = pd.to_datetime(date_fmt) - pd.Timedelta(days=1)
-            date_fmt = dt.strftime('%Y-%m-%d')
-            
-    #--max temp (Tmax.tif)
-    #Reset loop flag and date and attempts
-    date_fmt = targ_dt.strftime('%Y-%m-%d')
-    finish_flag = 0
-    attempts = 0
-    print(f'Getting Tmax for {date_fmt}')
-    while (finish_flag < 1)&(attempts<10):
-        tmax_url = f"https://api.hcdp.ikewai.org/raster?date={date_fmt}&extent={county}&datatype=temperature&aggregation=max&period=day&returnEmptyNotFound=False"
-        tmax_file = local_dep_dir + 'Tmax_'+county+'.tif'
-        try:
-            req = requests.get(tmax_url,headers={'Authorization':f'Bearer {hcdp_api_token}'})
-            req.raise_for_status()
-            with open(tmax_file,'wb') as f:
-                f.write(req.content)
-            finish_flag = 1
-            attempts += 1
-        except requests.exceptions.HTTPError as err:
-            print(f'Tmax for {date_fmt} not found. Fetching previous day.')
-            dt = pd.to_datetime(date_fmt) - pd.Timedelta(days=1)
-            date_fmt = dt.strftime('%Y-%m-%d')
-
-    #--relhum (RH.tif)
-    #Reset loop flag and date and attempts
-    date_fmt = targ_dt.strftime('%Y-%m-%d')
-    finish_flag = 0
-    attempts = 0
-    print(f'Getting RH for {date_fmt}')
-    while (finish_flag < 1)&(attempts<10):
-        rh_url = f"https://api.hcdp.ikewai.org/raster?date={date_fmt}&extent={county}&datatype=relative_humidity&period=day&returnEmptyNotFound=False"
-        rh_file = local_dep_dir + 'RH_'+county+'.tif'
-        try:
-            req = requests.get(rh_url,headers={'Authorization':f'Bearer {hcdp_api_token}'})
-            req.raise_for_status()
-            with open(rh_file,'wb') as f:
-                f.write(req.content)
-            finish_flag = 1
-            attempts += 1
-        except requests.exceptions.HTTPError as err:
-            print(f'RH for {date_fmt} not found. Fetching previous day.')
-            dt = pd.to_datetime(date_fmt) - pd.Timedelta(days=1)
-            date_fmt = dt.strftime('%Y-%m-%d')
+    for dataset, fname_prefix in datasets:
+        outf = join(local_dep_dir, f"{fname_prefix}_{county}.tif")
+        targ_dt = get_last_raster(targ_dt, county, dataset, outf)
